81204b5e04c97f85d967b908770228ed18f35767
Max Halford <maxhalford25@gmail.com>
2017-11-22 17:37:54 +0100

    Add NewEstimatorWithDefaults method

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,28 +7,7 @@ import (
 )
 
 func BenchmarkFit(b *testing.B) {
-	var estimator, err = NewEstimator(
-		10,                // constMax
-		-10,               // constMin
-		"mae",             // evalMetric
-		"sum,sub,mul,div", // funcs
-		10,                // generations
-		"mae",             // lossMetric
-		6,                 // maxHeight
-		3,                 // minHeight
-		1,                 // nPops
-		0,                 // parsimonyCoeff
-		0.5,               // pConstant
-		0.5,               // pFull
-		0.01,              // pHoistMutation
-		0.01,              // pPointMutation
-		0.9,               // pSubTreeCrossover
-		0.01,              // PSubTreeMutation
-		0.3,               // pTerminal
-		30,                // popSize
-		42,                // seed
-		0,                 // tuningGenerations
-	)
+	var estimator, err = NewEstimatorWithDefaults()
 	if err != nil {
 		panic(err)
 	}
--- estimator.go
+++ estimator.go
@@ -405,3 +405,33 @@ func NewEstimator(
 
 	return estimator, nil
 }
+
+// NewEstimatorWithDefaults call NewEstimator with default values.
+func NewEstimatorWithDefaults() (*Estimator, error) {
+	var estimator, err = NewEstimator(
+		10,                // constMax
+		-10,               // constMin
+		"mae",             // evalMetric
+		"sum,sub,mul,div", // funcs
+		10,                // generations
+		"mae",             // lossMetric
+		6,                 // maxHeight
+		3,                 // minHeight
+		1,                 // nPops
+		0,                 // parsimonyCoeff
+		0.5,               // pConstant
+		0.5,               // pFull
+		0.01,              // pHoistMutation
+		0.01,              // pPointMutation
+		0.9,               // pSubTreeCrossover
+		0.01,              // PSubTreeMutation
+		0.3,               // pTerminal
+		30,                // popSize
+		42,                // seed
+		0,                 // tuningGenerations
+	)
+	if err != nil {
+		return nil, err
+	}
+	return estimator, nil
+}
