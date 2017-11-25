6eb178a857ae2d4c3e7d7738984bc3b90c72a678
Max Halford <maxhalford25@gmail.com>
2017-11-25 19:13:19 +0100

    Add Go coverage for travis

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,6 +3,6 @@
 if [ ${TASK} == "go_test" ]; then
     set -e
     go get github.com/mattn/goveralls
-    go test `go list ./... | grep -v -e cmd -e python-package`
+    go test -race -cover -coverprofile=coverage.out `go list ./... | grep -v -e cmd -e python-package`
     ${HOME}/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci
 fi
