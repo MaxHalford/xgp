ca1c00641d9a2bb4e3750aff90f87b285e2d8453
Max Halford <maxhalford25@gmail.com>
2017-12-02 17:42:46 +0100

    Fix Travis

--- .travis/make_documentation.sh
+++ .travis/make_documentation.sh
@@ -1,3 +1,4 @@
-sudo apt-get install python
-sudo pip install mkdocs mkdocs-material
+conda create -n mkdocs python=3.5
+source activate mkdocs
+pip install mkdocs mkdocs-material
 mkdocs build --verbose --clean --strict
