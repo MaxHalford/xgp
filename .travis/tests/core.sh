3ef3b7e9c47559ee2cc538eb9e9c79cab51373e4
MaxHalford <maxhalford25@gmail.com>
2018-05-22 10:39:22 +0200

    Change go 1.9 version

--- .travis/tests/core.sh
+++ .travis/tests/core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 go get github.com/mattn/goveralls
-go get `go list ./... | grep -v -e cmd -e python-package`
-go test -race -cover -coverprofile=coverage.out `go list ./... | grep -v -e cmd -e python-package`
+go get `go list ./... | grep -v -e cmd`
+go test -race -cover -coverprofile=coverage.out `go list ./... | grep -v -e cmd`
 ${HOME}/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci
