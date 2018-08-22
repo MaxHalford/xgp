3cf4486552d0ba1a45b6111ae75298bc078fd35d
Max Halford <maxhalford25@gmail.com>
2018-08-22 13:45:58 +0200

    Use miniconda for docs

--- .travis/make_documentation.sh
+++ .travis/make_documentation.sh
@@ -1,3 +1,20 @@
-sudo apt-get install python
-sudo pip3 install mkdocs mkdocs-material python-markdown-math
+#!/bin/bash
+
+mkdir -p download
+cd download
+echo "Cached in $HOME/download :"
+ls -l
+echo
+if [[ ! -f miniconda.sh ]]
+    then
+        if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+            wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+        else
+            wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        fi
+    fi
+chmod +x miniconda.sh && ./miniconda.sh -b
+export PATH=/home/travis/miniconda3/bin:$PATH
+pip install mkdocs mkdocs-material python-markdown-math
+cd ..
 mkdocs build --verbose --clean --strict
